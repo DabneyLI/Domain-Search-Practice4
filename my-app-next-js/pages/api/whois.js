8fe10eae7e3f32d6bc9313aa1a8da1ab90012258
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-12-18 22:38:36 +0100

    Update whois.js

--- my-app-next-js/pages/api/whois.js
+++ my-app-next-js/pages/api/whois.js
@@ -5,49 +5,50 @@ export default async function handler(req, res) {
     const [name, suffix] = query.split('.');
 
     try {
-        // 检查最近24小时内是否有查询记录
-        let { data: cachedData, error: cachedError } = await supabase
+        // Step 1: Compare search input with each existing item of domain column in Supabase table
+        let { data: records, error } = await supabase
             .from('queries')
             .select('*')
-            .eq('domain', query)
-            .gte('timestamp', new Date(new Date().getTime() - 24*60*60*1000).toISOString()) // 最近24小时
-            .limit(1); // 只获取最近的一条记录
+            .eq('domain', query);
 
-        if (cachedError) {
-            console.error('Error fetching from cache:', cachedError);
-            return res.status(500).json({ error: cachedError.message });
+        if (error) {
+            console.error('Error fetching from Supabase:', error);
+            return res.status(500).json({ error: error.message });
         }
 
-        // 如果缓存中有记录，并且域名已注册，则直接返回缓存的数据
-        if (cachedData && cachedData.length > 0 && cachedData[0].registered) {
-            return res.status(200).json({ domain: query, isRegistered: true });
-        }
+        // Step 2: Check if any record matches and is within the last 24 hours
+        const recentRecord = records.find(record => 
+            new Date() - new Date(record.timestamp) < 86400000 // 24 hours in milliseconds
+        );
 
-        // 如果没有缓存记录或域名未注册，则调用 WHOIS 接口
-        const apiUrl = `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`;
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch WHOIS data: ${response.statusText}`);
-        }
-        const data = await response.json();
+        if (recentRecord) {
+            // Step 3: Return domain and registered status, no new record in Supabase
+            return res.status(200).json({ domain: query, isRegistered: recentRecord.registered });
+        } else {
+            // Step 4: Perform a WHOIS lookup and log data into Supabase
+            const apiUrl = `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`;
+            const response = await fetch(apiUrl);
+        
+            if (!response.ok) {
+                throw new Error(`Failed to fetch WHOIS data: ${response.statusText}`);
+            }
+        
+            const whoisData = await response.json();
+            const isRegistered = whoisData.status !== 'available';
 
-        // 根据 WHOIS 接口的响应，确定域名是否已注册
-        const isRegistered = data.status !== 'available';
-
-        // 保存新的查询结果到 Supabase
-        if (!cachedData || cachedData.length === 0) {
-            const { error } = await supabase.from('queries').insert([
-                { domain: query, registered: isRegistered }
+            // Log the new data into Supabase
+            const { error: insertError } = await supabase.from('queries').insert([
+                { domain: query, registered: isRegistered, timestamp: new Date().toISOString() }
             ]);
 
-            if (error) {
-                console.error('Error saving to Supabase:', error);
-                throw error;
+            if (insertError) {
+                console.error('Error saving to Supabase:', insertError);
+                throw insertError;
             }
-        }
 
-        // 返回 WHOIS 查询结果
-        res.status(200).json({ domain: query, isRegistered });
+            // Return the WHOIS full response
+            res.status(200).json({ domain: query, whoisData, isRegistered });
+        }
     } catch (error) {
         console.error('API route error:', error);
         res.status(500).json({ error: error.message });
