dad0f64ab185c02cac8c6c17e82b36649c985672
dabneyli <144627567+DabneyLI@users.noreply.github.com>
2023-12-18 14:31:29 +0000

    Update 4 files

--- my-app-next-js/pages/index.js
+++ my-app-next-js/pages/index.js
@@ -1,61 +1,43 @@
 import React, { useEffect, useState } from "react";
-import queryDomain from "./api/whois";
 import { supabase } from "../utils/supabaseClient";
 
-const DomainChecker = () => {
-  const [domain, setDomain] = useState("");
-  const [recentDomains, setRecentDomains] = useState([]);
+export default function Home() {
+  const [history, setHistory] = useState([]);
 
   useEffect(() => {
-    getRecentDomains().then(setRecentDomains);
-  }, []);
+    async function fetchHistory() {
+      const { data, error } = await supabase
+        .from("queries")
+        .select("*")
+        .order("timestamp", { ascending: false })
+        .limit(20);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const [name, suffix] = domain.split(".");
-    const whoisData = await queryDomain(name, suffix);
-    window.open(
-      `data:text/json,${encodeURIComponent(JSON.stringify(whoisData))}`,
-      "_blank",
-    );
-  };
-
-  async function getRecentDomains() {
-    let { data: domains, error } = await supabase
-      .from("queries")
-      .select("domain, registered")
-      .order("timestamp", { ascending: false })
-      .limit(20);
-
-    if (error) {
-      console.error("Error fetching recent domains:", error);
-      return [];
+      if (error) {
+        console.error("Error fetching history:", error);
+      } else {
+        setHistory(data);
+      }
     }
 
-    return domains.filter((d) => d.registered); // 只返回已注册的域名
-  }
+    fetchHistory();
+  }, []);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={domain}
-          onChange={(e) => setDomain(e.target.value)}
-          placeholder="请输入域名"
-        />
-        <button type="submit">查询</button>
+      <h1>WHOIS Query</h1>
+      <form action="/api/whois" method="get" target="_blank">
+        <input name="query" type="search" placeholder="Enter domain" required />
+        <button type="submit">Search</button>
       </form>
-      <div>
-        最近查询的域名:
-        {recentDomains.map((d, i) => (
-          <div key={i}>
-            {d.domain} - {d.registered ? "已注册" : "未注册"}
-          </div>
+
+      <h2>Recent Queries</h2>
+      <ul>
+        {history.map((item, index) => (
+          <li key={index}>
+            {item.domain} - {item.registered ? "Registered" : "Not Registered"}
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
-};
-
-export default DomainChecker;
+}
