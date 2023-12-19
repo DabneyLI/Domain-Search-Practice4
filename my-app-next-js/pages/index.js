dc1762981c58836504f84ed06555e1829664dbef
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-12-19 11:21:47 +0100

    Update index.js

--- my-app-next-js/pages/index.js
+++ my-app-next-js/pages/index.js
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { supabase } from "../utils/supabaseClient";
+// Inside your index.js or wherever your search component is
 
-export default function Home() {
+// ... other imports
+import { useEffect, useState } from 'react';
+
+const fetchHistory = async () => {
+  // Fetch the updated history and set it in the state
+  const response = await fetch('/api/history');
+  const data = await response.json();
+  if (response.ok) {
+    setHistory(data);
+  } else {
+    console.error('Error fetching history');
+  }
+};
+
+const SearchComponent = () => {
+  // ... existing state and other hooks
+
   const [history, setHistory] = useState([]);
 
   useEffect(() => {
-    async function fetchHistory() {
-      const { data, error } = await supabase
-        .from("queries")
-        .select("*")
-        .order("timestamp", { ascending: false })
-        .limit(20);
-
-      if (error) {
-        console.error("Error fetching history:", error);
-      } else {
-        setHistory(data);
-      }
-    }
-
+    // Initially fetch the history when the component mounts
     fetchHistory();
   }, []);
 
-  return (
-    <div>
-      <h1>WHOIS Query</h1>
-      <form action="/api/whois" method="get" target="_blank">
-        <input name="query" type="search" placeholder="Enter domain" required />
-        <button type="submit">Search</button>
-      </form>
+  const performSearch = async (domain) => {
+    try {
+      // Perform the WHOIS lookup
+      const response = await fetch('/api/whois?query=' + domain);
+      const data = await response.json();
+      if (response.ok) {
+        // If the search was successful, refresh the history
+        fetchHistory();
+      } else {
+        console.error('Error during WHOIS lookup:', data.error);
+      }
+    } catch (error) {
+      console.error('Error performing search:', error);
+    }
+  };
 
-      <h2>Recent Queries</h2>
-      <ul>
-        {history.map((item, index) => (
-          <li key={index}>
-            {item.domain} - {item.registered ? "Registered" : "Not Registered"}
-          </li>
-        ))}
-      </ul>
-    </div>
+  // ... your JSX including the search input and submit button
+
+  return (
+    // ... your JSX
   );
-}
+};
+
+export default SearchComponent;
