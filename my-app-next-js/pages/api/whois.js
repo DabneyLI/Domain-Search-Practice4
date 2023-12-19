0624bb6a9928e9994ed71e1b75202008830cac1b
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-12-19 11:41:52 +0100

    Update whois.js

--- my-app-next-js/pages/api/whois.js
+++ my-app-next-js/pages/api/whois.js
@@ -1,76 +1,68 @@
+// pages/api/whois.js
 import { supabase } from '../../utils/supabaseClient';
 
 export default async function handler(req, res) {
-    const { query } = req.query;
-    const [name, suffix] = query.split('.');
+  const { domain } = req.query;
 
-    try {
-        // Compare search input with each existing item of domain column in Supabase table
-        let { data: records, error } = await supabase
-            .from('queries')
-            .select('*')
-            .eq('domain', query);
+  // Perform validation on the domain, if needed
+  if (!domain) {
+    return res.status(400).json({ error: 'Domain is required' });
+  }
 
-        if (error) {
-            console.error('Error fetching from Supabase:', error);
-            return res.status(500).json({ error: error.message });
-        }
+  try {
+    // Fetch the record from the Supabase database
+    const { data: existingRecords, error: fetchError } = await supabase
+      .from('queries')
+      .select('*')
+      .eq('domain', domain)
+      .single();
 
-        // Check if any record matches and is within the last 24 hours
-        const recentRecord = records.find(record => 
-            new Date() - new Date(record.timestamp) < 86400000 // 24 hours in milliseconds
-        );
-
-        if (recentRecord) {
-            // Update the timestamp of the recent record
-            const updatedTimestamp = new Date().toISOString();
-            const { error: updateError } = await supabase
-                .from('queries')
-                .update({ timestamp: updatedTimestamp })
-                .match({ id: recentRecord.id }); // Assuming the table has an 'id' column
-
-            if (updateError) {
-                console.error('Error updating timestamp in Supabase:', updateError);
-                throw updateError;
-            }
-
-            // Return domain and its registered status, with updated timestamp
-            return res.status(200).json({ 
-                domain: query, 
-                isRegistered: recentRecord.registered,
-                updatedTimestamp
-            });
-        } else {
-            // Perform a WHOIS lookup and log data into Supabase
-            const apiUrl = `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`;
-            const response = await fetch(apiUrl);
-        
-            if (!response.ok) {
-                throw new Error(`Failed to fetch WHOIS data: ${response.statusText}`);
-            }
-        
-            const whoisData = await response.json();
-            const isRegistered = whoisData.status !== 'available';
-
-            // Log the new data into Supabase
-            const newRecord = {
-                domain: query, 
-                registered: isRegistered, 
-                timestamp: new Date().toISOString()
-            };
-
-            const { error: insertError } = await supabase.from('queries').insert([newRecord]);
-
-            if (insertError) {
-                console.error('Error saving to Supabase:', insertError);
-                throw insertError;
-            }
-
-            // Return the WHOIS full response
-            res.status(200).json({ domain: query, whoisData, isRegistered });
-        }
-    } catch (error) {
-        console.error('API route error:', error);
-        res.status(500).json({ error: error.message });
+    if (fetchError && fetchError.message !== 'No rows found') {
+      throw fetchError;
     }
+
+    const record = existingRecords;
+    const currentTime = new Date().toISOString();
+
+    if (record && new Date() - new Date(record.created_at) < 86400000) {
+      // Record is less than 24 hours old. Update the timestamp.
+      const { error: updateError } = await supabase
+        .from('queries')
+        .update({ updated_at: currentTime })
+        .eq('id', record.id);
+
+      if (updateError) {
+        throw updateError;
+      }
+
+      return res.status(200).json({ message: 'Record timestamp updated', domain });
+    } else {
+      // No record or record is more than 24 hours old. Perform a new WHOIS lookup and insert a new record.
+
+      // Dummy WHOIS lookup result - replace this with your actual WHOIS lookup logic
+      const whoisResult = {
+        status: 'available' // or 'registered', based on your WHOIS lookup
+      };
+
+      const { error: insertError } = await supabase
+        .from('queries')
+        .insert([
+          {
+            domain,
+            registered: whoisResult.status === 'registered',
+            created_at: currentTime,
+            updated_at: currentTime
+          }
+        ]);
+
+      if (insertError) {
+        throw insertError;
+      }
+
+      return res.status(200).json({ message: 'New record created', domain, whoisResult });
+    }
+  } catch (error) {
+    console.error('Error in WHOIS API:', error);
+    res.status(500).json({ error: error.message });
+  }
 }
