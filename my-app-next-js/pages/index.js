e1a94241a4efa4ce1e1b364f0223f4170338b235
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-12-19 13:55:39 +0100

    Update index.js

--- my-app-next-js/pages/index.js
+++ my-app-next-js/pages/index.js
@@ -3,48 +3,30 @@ import { supabase } from "../utils/supabaseClient";
 
 export default function Home() {
   const [history, setHistory] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
+    async function fetchHistory() {
+      const { data, error } = await supabase
+        .from("queries")
+        .select("*")
+        .order("timestamp", { ascending: false })
+        .limit(20);
+
+      if (error) {
+        console.error("Error fetching history:", error);
+      } else {
+        setHistory(data);
+      }
+    }
+
     fetchHistory();
   }, []);
-
-  const fetchHistory = async () => {
-    const { data, error } = await supabase
-      .from("queries")
-      .select("*")
-      .order("timestamp", { ascending: false })
-      .limit(20);
-
-    if (error) {
-      console.error("Error fetching history:", error);
-    } else {
-      setHistory(data);
-    }
-  };
-
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    const response = await fetch(`/api/whois?query=${searchQuery}`);
-    if (response.ok) {
-      fetchHistory(); // Refresh history after search
-    } else {
-      console.error("Error performing WHOIS lookup");
-    }
-  };
 
   return (
     <div>
       <h1>WHOIS Query</h1>
-      <form onSubmit={handleSearch}>
-        <input 
-          name="query" 
-          type="search" 
-          placeholder="Enter domain" 
-          required 
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
+      <form action="/api/whois" method="get" target="_blank">
+        <input name="query" type="search" placeholder="Enter domain" required />
         <button type="submit">Search</button>
       </form>
 
